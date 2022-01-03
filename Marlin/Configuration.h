df42ee488c6a6e2418fc1027c6e87e34f404e178
ClockeNessMnstr <locke.dftc@gmail.com>
2022-01-03 17:35:48 -0500

    update Ymax

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1400,7 +1400,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 390
-#define Y_BED_SIZE 380
+#define Y_BED_SIZE 390
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
